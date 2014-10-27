2888febf55c7a4e19707a88486e78905f4603b4f
manasdk <manas@stackstorm.com>
2014-10-27 21:55:08 +0000

    create ec2 client in baseaction.

--- packs/aws/actions/lib/action.py
+++ packs/aws/actions/lib/action.py
@@ -1,7 +1,11 @@
+from lib import ec2
+
+
 class BaseAction(object):
 
     def __init__(self, config):
         super(BaseAction, self).__init__()
-        self.region = self.config['region']
-        self.access_key_id = self.config['access_key_id']
-        self.secret_access_key = self.config['secret_access_key']
+        region = self.config['region']
+        access_key_id = self.config['access_key_id']
+        secret_access_key = self.config['secret_access_key']
+        self.ec2 = ec2.EC2(region, access_key_id, secret_access_key)
