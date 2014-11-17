a354b08812b4ff0b1cd25a8f6a618708db7429ab
Tomaz Muraus <tomaz@tomaz.me>
2014-11-17 14:52:14 +0800

    Update docker sensor so it implements "poll" method which is required by the new StackStorm versions.

--- packs/docker/sensors/docker_container_sensor.py
+++ packs/docker/sensors/docker_container_sensor.py
@@ -16,6 +16,12 @@ class DockerSensor(object):
         self._poll_interval = 5  # seconds
         self._trigger_pack = 'docker'
 
+        started_trigger = self.get_trigger_types()[0]
+        stopped_trigger = self.get_trigger_types()[1]
+
+        self._started_trigger_ref = '.'.join([self._trigger_pack, started_trigger['name']])
+        self._stopped_trigger_ref = '.'.join([self._trigger_pack, stopped_trigger['name']])
+
     def setup(self):
         docker_opts = self._config
 
@@ -37,36 +43,41 @@ class DockerSensor(object):
         self._running_containers = self._get_active_containers()
         self._poll_interval = docker_opts.get('poll_interval', self._poll_interval)
 
+    def poll(self):
+        containers = self._get_active_containers()
+
+        # Stopped
+        for id, running_container in six.iteritems(self._running_containers):
+            if id not in containers:
+                self._dispatch_trigger(trigger=self._stopped_trigger_ref,
+                                       container=running_container)
+
+        # Started
+        for id, container in six.iteritems(containers):
+            if id not in self._running_containers:
+                self._dispatch_trigger(trigger=self._started_trigger_ref,
+                                       container=container)
+
+        self._running_containers = containers
+
     def start(self):
-        started_trigger  = self.get_trigger_types()[0]
-        stopped_trigger = self.get_trigger_types()[1]
-
-        started_trigger_ref = '.'.join([self._trigger_pack, started_trigger['name']])
-        stopped_trigger_ref = '.'.join([self._trigger_pack, stopped_trigger['name']])
-
-        while True:
-            containers = self._get_active_containers()
-
-            # Stopped
-            for id, running_container in six.iteritems(self._running_containers):
-                if id not in containers:
-                    self._dispatch_trigger(trigger=stopped_trigger_ref,
-                                           container=running_container)
-
-            # Started
-            for id, container in six.iteritems(containers):
-                if id not in self._running_containers:
-                    self._dispatch_trigger(trigger=started_trigger_ref,
-                                           container=container)
-
-            self._running_containers = containers
-            time.sleep(self._poll_interval)
+        """
+        Note: This method is only needed for StackStorm v0.5. Newer versions of
+        StackStorm, only require sensor to implement "poll" method and the
+        actual poll schedueling is handled outside of the sensor class.
+        """
+        self.poll()
+        time.sleep(self._poll_interval)
 
     def stop(self):
         if getattr(self._client, 'close') is not None:
             self._client.close()
 
     def get_trigger_types(self):
+        """
+        Note: This method is only needed for StackStorm v0.5. In newer versions,
+        trigger_types are defined in the sensor metadata file.
+        """
         return [
             {
                 'name': 'container_tracker.started',
