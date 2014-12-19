6235c7f6edbdc19e697899a75e87e407357b6a12
Sebastien Goasguen <runseb@gmail.com>
2014-12-19 05:46:45 -0500

    Add exoscale provider to libcloud pack

--- packs/libcloud/actions/create_vm.py
+++ packs/libcloud/actions/create_vm.py
@@ -12,19 +12,27 @@ __all__ = [
 class CreateVMAction(BaseAction):
     api_type = 'compute'
 
-    def run(self, credentials, name, size_id, image_id, location_id=None):
+    def run(self, credentials, name, size_id=None, image_id=None,
+            size_name=None, image_name=None, location_id=None):
         driver = self._get_driver_for_credentials(credentials=credentials)
-        size = NodeSize(id=size_id, name=None,
-                        ram=None, disk=None, bandwidth=None,
-                        price=None, driver=driver)
-        image = NodeImage(id=image_id, name=None,
-                          driver=driver)
         location = NodeLocation(id=location_id, name=None,
                                 country=None, driver=driver)
-
         self.logger.info('Creating node...')
 
-        kwargs = {'name': name, 'size': size, 'image': image}
+        if size_id != None and image_id != None:
+            image = NodeImage(id=image_id, name=None,
+                              driver=driver)
+            size = NodeSize(id=size_id, name=None,
+                            ram=None, disk=None, bandwidth=None,
+                            price=None, driver=driver)
+
+            kwargs = {'name': name, 'size': size, 'image': image}
+
+        if size_name != None and image_name != None:
+            image = [i for i in driver.list_images() if i.extra['displaytext'] == image_name][0]
+            size = [s for s in driver.list_sizes() if s.name == size_name][0]
+
+            kwargs = {'name': name, 'size': size, 'image': image}
 
         if location_id:
             kwargs['location'] = location
