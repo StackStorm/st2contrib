e504447938775558d8fd78599f96904a080b5ad3
Patrick Hoolboom <patrick@stackstorm.com>
2014-12-30 12:45:07 -0800

    Added pkill and wait_for_ssh

---
+++ packs/linux/actions/wait_for_ssh.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import paramiko
+import argparse
+from st2actions.runners.pythonrunner import Action
+import os, yaml, json, time
+
+class BaseAction(Action):
+
+    def __init__(self, config):
+        super(BaseAction, self).__init__(config)
+
+    def run(self,keyfile,username,hostname,timeout,retries):
+ 
+        key = paramiko.RSAKey.from_private_key_file(keyfile)
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        for x in range(retries):
+            try:
+                client.connect( hostname = hostname, username = username, pkey = key )
+                return True
+            except Exception, e: 
+                self.logger.info(e)
+                time.sleep(timeout)
+            time.sleep(20)
