b3790a607a9f48561ce7a3da9242927510974808
Tomaz Muraus <tomaz@tomaz.me>
2015-01-28 21:13:48 +0100

    Make sure region name is uppercase.

--- packs/rackspace/actions/lib/action.py
+++ packs/rackspace/actions/lib/action.py
@@ -1,9 +1,11 @@
-from st2actions.runners.pythonrunner import Action
 import pyrax
+
+from st2actions.runners.pythonrunner import Action
 
 __all__ = [
     'PyraxBaseAction'
 ]
+
 
 class PyraxBaseAction(Action):
     def __init__(self, config):
@@ -15,7 +17,9 @@ class PyraxBaseAction(Action):
         api_key = self.config['api_key']
 
         # Needs to be extracted to per-action
-        region = self.config['region']
+        region = self.config['region'].upper()
+
+        print 'xxx', region
 
         pyrax.set_setting('identity_type', 'rackspace')
         pyrax.set_default_region(region)
