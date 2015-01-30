af9daf9420ef9693d3a9081315f7fc2a9e30594a
Tomaz Muraus <tomaz@tomaz.me>
2015-01-30 11:00:14 +0100

    Style and lint fixes.

--- packs/linux/actions/get_open_ports.py
+++ packs/linux/actions/get_open_ports.py
@@ -6,23 +6,32 @@ from st2actions.runners.pythonrunner import Action
 Note 1: This action requires nmap binary to be available.
 
 Note 2: We only scan for open TCP ports since scanning for open UDP ports
-        (-sU) requires root priveleges.
+                (-sU) requires root priveleges.
 """
 
 
 class PortScanner(Action):
-  def run(self, host):
-    result = []
-    port_details = {}
-    ps = nmap.PortScanner()
-    scan_res = ps.scan(host, arguments='--min-parallelism 100 -sT')
-    for target_host in ps.all_hosts():
-      if target_host in ps.all_hosts():
-        for comm in ps[target_host].all_protocols():
-           if comm in ['tcp','udp','ip','sctp']:
-             ports = ps[target_host][comm].keys()
-             ports.sort()
-             for port in ports:
-               port_details = {port:{'state':ps[host][comm][port]['state'], 'service':ps[host][comm][port]['name'], 'protocol':comm}}
-               result.append(port_details)
-    return result
+    def run(self, host):
+        result = []
+        port_details = {}
+        ps = nmap.PortScanner()
+        ps.scan(host, arguments='--min-parallelism 100 -sT')
+        for target_host in ps.all_hosts():
+            if target_host not in ps.all_hosts():
+                continue
+
+            for comm in ps[target_host].all_protocols():
+                if comm in ['tcp', 'udp', 'ip', 'sctp']:
+                    ports = ps[target_host][comm].keys()
+                    ports.sort()
+                    for port in ports:
+                        port_details = {
+                            port: {
+                                'state': ps[host][comm][port]['state'],
+                                'service': ps[host][comm][port]['name'],
+                                'protocol': comm
+                            }
+                        }
+                        result.append(port_details)
+
+        return result
