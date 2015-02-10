ffa352d3d0bea7dbc4cf3cf97cb8e4f48d485396
Tomaz Muraus <tomaz@tomaz.me>
2015-02-10 21:18:33 +0100

    Add gpg.import_keys action.

---
+++ packs/gpg/actions/import_keys.py
@@ -0,0 +1,14 @@
+from lib.base import BaseGPGAction
+
+__all__ = [
+    'ImportKeysAction'
+]
+
+
+class ImportKeysAction(BaseGPGAction):
+    def run(self, file_path):
+        with open(file_path, 'r') as fp:
+            key_data = fp.read()
+
+        import_result = self._gpg.import_keys(key_data)
+        return import_result.count
