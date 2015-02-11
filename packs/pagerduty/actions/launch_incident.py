c0923d248ef87a94518bd75a88d9421ae5d17f33
aamir <raza.aamir01@gmail.com>
2015-02-11 12:50:25 -0500

    fixing arg

--- packs/pagerduty/actions/launch_incident.py
+++ packs/pagerduty/actions/launch_incident.py
@@ -2,7 +2,7 @@ from lib.action import PagerDutyAction
 
 
 class LaunchIncident(PagerDutyAction):
-  def run(self, description, details):
+  def run(self, description, details=None):
     """lauch a trigger """
     lst = self.pager.trigger_incident(self.config['service_api'], description=description, details=details) 
     return lst
