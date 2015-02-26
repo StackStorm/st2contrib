8b2721362877f1a66ced091eeaa9160f83a4f7c6
Daniel Wallace <danielwallace@gtmanfred.com>
2015-02-26 16:39:11 -0600

    add example of salt.client

--- packs/salt/actions/client.py
+++ packs/salt/actions/client.py
@@ -6,6 +6,13 @@ from st2actions.runners.pythonrunner import Action
 
 class SaltClientAction(Action):
     def run(self, matches, module, args=None, kwargs=None):
+        '''
+        CLI Examples:
+
+            st2 run salt.client matches='web*' module=test.ping
+            st2 run salt.client module=pkg.install \
+                    kwargs='{"pkgs":["git","httpd"]}'
+        '''
         if args is None:
             args = []
         if kwargs is None:
