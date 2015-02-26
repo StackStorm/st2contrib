e894cdae9916c7f4a9c40a85672bd1ab1fea9b59
Daniel Wallace <danielwallace@gtmanfred.com>
2015-02-26 15:17:37 -0600

    add kwargs for positional arguments

--- packs/salt/actions/client.py
+++ packs/salt/actions/client.py
@@ -5,10 +5,11 @@ from st2actions.runners.pythonrunner import Action
 
 
 class SaltClientAction(Action):
-    def run(self, matches, module, arguments=None):
-        if arguments is None:
-            arguments = []
-
+    def run(self, matches, module, args=None, kwargs=None):
+        if args is None:
+            args = []
+        if kwargs is None:
+            kwargs = {}
         cli = salt.client.LocalClient()
-        ret = cli.cmd(matches, module, arguments)
+        ret = cli.cmd(matches, module, arg=args, kwarg=kwargs)
         return ret
