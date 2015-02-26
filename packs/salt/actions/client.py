71a007a3c92a3351f6c83339868e8e57402f9b2c
Daniel Wallace <danielwallace@gtmanfred.com>
2015-02-26 15:08:50 -0600

    fix if arguments is empty

--- packs/salt/actions/client.py
+++ packs/salt/actions/client.py
@@ -1,10 +1,14 @@
+import copy
 import salt.client
 
 from st2actions.runners.pythonrunner import Action
 
 
 class SaltClientAction(Action):
-    def run(self, matches, module, arguments):
+    def run(self, matches, module, arguments=None):
+        if arguments is None:
+            arguments = []
+
         cli = salt.client.LocalClient()
-        ret = cli.cmd(matches, module, **arguments)
+        ret = cli.cmd(matches, module, arguments)
         return ret
