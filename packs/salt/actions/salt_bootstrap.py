dd73aa4fd1248e3e97ea5330c4ad7d61082a73d3
Daniel Wallace <danielwallace@gtmanfred.com>
2015-02-26 14:52:22 -0600

    add self...

--- packs/salt/actions/salt_bootstrap.py
+++ packs/salt/actions/salt_bootstrap.py
@@ -4,7 +4,7 @@ from st2actions.runners.pythonrunner import Action
 
 
 class SaltInstaller(Action):
-    def run(name, provider, instance_id):
+    def run(self, name, provider, instance_id):
         client = salt.cloud.CloudClient('/etc/salt/cloud')
         ret = client.create(names=[name], provider=provider,
                             instance_id=instance_id)
