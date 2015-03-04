81980a8fb2f0543a776ef22ee9d8416cf609337e
Tomaz Muraus <tomaz@tomaz.me>
2015-03-04 15:41:46 +0100

    Move common formatting functionality in a separate module.

--- packs/jira/actions/get_issue.py
+++ packs/jira/actions/get_issue.py
@@ -1,4 +1,5 @@
 from lib.base import BaseJiraAction
+from lib.formatters import to_issue_dict
 
 __all__ = [
     'GetJiraIssueAction'
@@ -8,38 +9,5 @@ __all__ = [
 class GetJiraIssueAction(BaseJiraAction):
     def run(self, issue_key):
         issue = self._client.issue(issue_key)
-
-        split = issue.permalink().split(' - ', 1)
-        url = split[0]
-
-        if issue.fields.resolution:
-            resolution = issue.fields.resolution.name
-        else:
-            resolution = None
-
-        if issue.fields.reporter:
-            reporter = issue.fields.reporter.displayName
-        else:
-            reporter = None
-
-        if issue.fields.assignee:
-            assignee = issue.fields.assignee.displayName
-        else:
-            assignee = None
-
-        result = {
-            'id': issue.id,
-            'key': issue_key,
-            'url': url,
-            'summary': issue.fields.summary,
-            'description': issue.fields.description,
-            'status': issue.fields.status.name,
-            'resolution': resolution,
-            'labels': issue.fields.labels,
-            'reporter': reporter,
-            'assignee': assignee,
-            'created_at': issue.fields.created,
-            'updated_at': issue.fields.updated,
-            'resolved_at': issue.fields.resolutiondate
-        }
+        result = to_issue_dict(issue=issue)
         return result
---
+++ packs/jira/actions/lib/formatters.py
@@ -0,0 +1,43 @@
+__all__ = [
+    'to_issue_dict'
+]
+
+
+def to_issue_dict(issue):
+    """
+    :rtype: ``dict``
+    """
+    split = issue.permalink().split(' - ', 1)
+    url = split[0]
+
+    if issue.fields.resolution:
+        resolution = issue.fields.resolution.name
+    else:
+        resolution = None
+
+    if issue.fields.reporter:
+        reporter = issue.fields.reporter.displayName
+    else:
+        reporter = None
+
+    if issue.fields.assignee:
+        assignee = issue.fields.assignee.displayName
+    else:
+        assignee = None
+
+    result = {
+        'id': issue.id,
+        'key': issue.key,
+        'url': url,
+        'summary': issue.fields.summary,
+        'description': issue.fields.description,
+        'status': issue.fields.status.name,
+        'resolution': resolution,
+        'labels': issue.fields.labels,
+        'reporter': reporter,
+        'assignee': assignee,
+        'created_at': issue.fields.created,
+        'updated_at': issue.fields.updated,
+        'resolved_at': issue.fields.resolutiondate
+    }
+    return result
