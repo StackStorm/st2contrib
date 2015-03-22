6a32852fdbd750e8727c9a0b34e1e47844850375
aamir <raza.aamir01@gmail.com>
2015-03-22 12:31:50 -0400

    used requests lib, changed coding style to be inline wiht PEP8

--- packs/victorops/actions/open_incident.py
+++ packs/victorops/actions/open_incident.py
@@ -1,17 +1,15 @@
-import urllib
+import requests
 from lib.action import VictorOpsAction
 import json
 import time
 
+
 class OpenIncident(VictorOpsAction):
-  def run(self, severity, entity, message=None):
-    prms = {
-    "message_type": severity,
-    "timestamp": int(time.time()),
-    "entity_id":entity,
-    "state_message":message
-    }
-    post_data = json.dumps(prms).encode()
-    data = urllib.urlopen(self.url, post_data)
-    
-   
+    def run(self, severity, entity, message=None):
+        prms = {
+                "message_type": severity,
+                "timestamp": int(time.time()),
+                "entity_id": entity,
+                "state_message": message}
+        post_data = json.dumps(prms)
+        data = requests.post(self.url, post_data)
