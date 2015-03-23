2f91daf88902060157f9b89c51305377425f3ebe
James Fryman <james@fryman.io>
2015-03-22 22:58:52 -0500

    Adding Initial Hue Support

---
+++ packs/hue/actions/color_temp_mired.py
@@ -0,0 +1,6 @@
+from lib import action
+
+class ColorTempMiredAction(action.BaseAction):
+    def run(self, light_id, temperature, transition_time):
+        light = self.hue.lights.get(light_id)
+        light.ct(temperature, transition_time)
