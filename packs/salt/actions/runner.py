fecbac47ce63ce409d4708d72e6af2ba4ad0b2f4
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-03-31 16:19:40 -0500

    First stab at action for salt runners

---
+++ packs/salt/actions/runner.py
@@ -0,0 +1,52 @@
+import copy
+import json
+import requests
+
+from st2actions.runners.pythonrunner import Action
+
+
+class SaltPacket(object):
+    _data = {"eauth": "pam",
+             "username": "",
+             "password": "",
+             "client": "",
+             "fun": ""}
+
+    def __init__(self, client='local'):
+        self._data['client'] = client
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, key_value=[]):
+        key, value = key_value
+        self._data[key] = value
+
+
+class SaltRunnerAction(Action):
+
+    def __init__(self, config):
+        super(SaltRunnerAction, self).__init__(config=config)
+        self.url = self._config.get('api_url', None)
+        self.username = self._config.get('username', None)
+        self.password = self._config.get('password', None)
+        self.data = SaltPacket('runner').data
+        self.data['username'] = self.username
+        self.data['password'] = self.password
+
+    def run(self, args=[], **kwargs):
+        '''
+        CLI Examples:
+
+            st2 run salt.runner manage.down
+            st2 run salt.runner manage.down removekeys=True
+        '''
+        self.data['fun'] = args[0]
+        resp = requests.post("{0}/run".format(self.url),
+                             headers={'content-type': 'application/json',
+                                      'charset': 'utf-8'},
+                             data=json.dumps(self.data),
+                             verify=True)
+        return resp.json()
