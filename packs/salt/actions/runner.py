f367c8e8cd7bbdbe3208f17b0fd9f25d9d93c309
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-04-01 18:14:11 -0500

    Runner action. Successfully calls runners through salt-api via the '/run' endpoint (no sessions).

--- packs/salt/actions/runner.py
+++ packs/salt/actions/runner.py
@@ -29,21 +29,25 @@ class SaltRunnerAction(Action):
 
     def __init__(self, config):
         super(SaltRunnerAction, self).__init__(config=config)
-        self.url = self._config.get('api_url', None)
-        self.username = self._config.get('username', None)
-        self.password = self._config.get('password', None)
+        self.url = self.config.get('api_url', None)
+        self.username = self.config.get('username', None)
+        self.password = self.config.get('password', None)
         self.data = SaltPacket('runner').data
         self.data['username'] = self.username
         self.data['password'] = self.password
 
-    def run(self, args=[], **kwargs):
+    def run(self, cmd, args=None, **kwargs):
         '''
         CLI Examples:
 
             st2 run salt.runner manage.down
             st2 run salt.runner manage.down removekeys=True
         '''
-        self.data['fun'] = args[0]
+        self.data['fun'] = cmd
+        if args:
+            self.data['arg'] = [args]
+        if kwargs:
+            self.data['kwargs'] = kwargs
         resp = requests.post("{0}/run".format(self.url),
                              headers={'content-type': 'application/json',
                                       'charset': 'utf-8'},
