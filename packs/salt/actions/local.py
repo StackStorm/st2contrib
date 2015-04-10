dcb66700b0f8d82776863ffcfdf69315e60fee32
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-04-10 09:38:26 -0500

    proxy action for generic execution module calls

---
+++ packs/salt/actions/local.py
@@ -0,0 +1,21 @@
+import json
+from requests import Session
+
+from lib.base import SaltAction
+
+
+class SaltLocal(SaltAction):
+
+    def run(self, **kwargs):
+        '''
+        CLI Examples:
+
+            st2 run salt.runner matches='web*' module=test.ping
+            st2 run salt.client module=pkg.install \
+                    kwargs='{"pkgs":["git","httpd"]}'
+        '''
+        self.generate_package(cmd=cmd)
+        request = self.generate_request()
+        request.prepare_body(json.dumps(self.data), None)
+        resp = Session().send(request, verify=True)
+        return resp.json()
