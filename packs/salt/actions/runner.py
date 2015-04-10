ca6c4ab7f47af4b59d045c13fcd37037385b65f4
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-04-10 09:36:01 -0500

    Added list if explicit runners

--- packs/salt/actions/runner.py
+++ packs/salt/actions/runner.py
@@ -6,18 +6,30 @@ from lib.base import SaltAction
 
 class SaltRunner(SaltAction):
 
-    def run(self, cmd, args=None, **kwargs):
+    __explicit__ = [
+        'jobs',
+        'manage',
+        'pillar',
+        'network'
+    ]
+
+    def run(self, **kwargs):
+        _cmd = None
+        if '.' not in kwargs['cmd']:
+            _cmd = '{0}.{1}'.format(kwargs['action'], kwargs['cmd'])
+        else:
+            _cmd = kwargs['cmd']
         '''
         CLI Examples:
 
-            st2 run salt.runner manage.down
-            st2 run salt.runner manage.down removekeys=True
+            st2 run salt.jobs active
+            st2 run salt.jobs jobs.list_jobs
         '''
-        self.generate_package('runner', cmd=cmd)
-        if args:
-            self.data['arg'] = [args]
-        if kwargs:
-            self.data['kwargs'] = kwargs
+        self.generate_package('runner', cmd=_cmd)
+        #if args:
+        #    self.data['arg'] = [args]
+        #if kwargs:
+        #    self.data['kwargs'] = kwargs
         request = self.generate_request()
         request.prepare_body(json.dumps(self.data), None)
         resp = Session().send(request, verify=True)
