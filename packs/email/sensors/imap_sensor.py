742162344c50630332b3fd5eb32c261e68fae76b
James Fryman <james@fryman.io>
2015-05-13 15:35:23 -0500

    Working IMAP Sensor

--- packs/email/sensors/imap_sensor.py
+++ packs/email/sensors/imap_sensor.py
@@ -1,6 +1,7 @@
 import eventlet
-import imbox
+import easyimap
 import json
+from flanker import mime
 
 from st2reactor.sensor.base import PollingSensor
 
@@ -23,23 +24,26 @@ class IMAPSensor(PollingSensor):
 
         self._trigger = 'email.imap.message'
         self._logger = self._sensor_service.get_logger(__name__)
+        self._mailboxes = {}
 
     def setup(self):
-        server = self._config.get('imap_server', 'localhost')
-        username = self._config['imap_username']
-        password = self._config['imap_password']
-        ssl = self._config['imap_ssl']
+        self._logger.debug('[IMAPSensor]: entering setup')
 
-        self._mailbox = imbox.Imbox(server, username, password, ssl)
+        if 'imap_mailboxes' in self._config:
+            self._parse_mailboxes(self._config['imap_mailboxes'])
 
     def poll(self):
-        unread_messages = self._mailbox.messages(unread=True)
-        for uid, message in unread_messages:
-            self._logger.debug('processing message %s' % uid)
-            self._process_message(message)
+        self._logger.debug('[IMAPSensor]: entering poll')
+
+        for name, mailbox in self._mailboxes.items():
+            self._poll_for_unread_messages(name, mailbox)
 
     def cleanup(self):
-        pass
+        self._logger.debug('[IMAPSensor]: entering cleanup')
+
+        for name, mailbox in self._mailboxes.items():
+            self._logger.debug('[IMAPSensor]: Disconnecting from {0}'.format(name))
+            mailbox.quit()
 
     def add_trigger(self, trigger):
         pass
@@ -50,25 +54,51 @@ class IMAPSensor(PollingSensor):
     def remove_trigger(self, trigger):
         pass
 
-    def _process_message(self, message):
-        self._logger.debug('posting message')
+    def _parse_mailboxes(self, mailboxes):
+        for mailbox, config in mailboxes.items():
+            server   = config.get('server', 'localhost')
+            port     = config.get('port', 143)
+            user     = config.get('username', None)
+            password = config.get('password', None)
+            folder   = config.get('mailbox', 'INBOX')
+            ssl      = config.get('ssl', False)
 
-        headers = json.dump(message['headers'])
-        body = json.dump(message['body'])
-        sent_from = json.dump(message['sent_from'])
-        sent_to = json.dump(message['sent_to'])
-        subject = message['subject']
+            if not user or not password:
+                self._logger.debug('[IMAPSensor]: Missing username/password for {0}'.format(mailbox))
+            elif not server:
+                self._logger.debug('[IMAPSensor]: Missing server for {0}'.format(mailbox))
+            else:
+                connection = easyimap.connect(server, user, password, folder, ssl=ssl, port=port)
+                self._mailboxes[mailbox] = connection
+
+    def _poll_for_unread_messages(self, name, mailbox):
+        self._logger.debug('[IMAPSensor]: polling mailbox {0}'.format(name))
+
+        for message in mailbox.unseen():
+            self._process_message(message.uid, mailbox)
+
+    def _process_message(self, uid, mailbox):
+        message = mailbox.mail(uid, include_raw=True)
+        mime_msg = mime.from_string(message.raw)
+
+        body = message.body
+        sent_from = message.from_addr
+        sent_to = message.to
+        subject = message.title
+        date = message.date
+        message_id = message.message_id
+        headers = mime_msg.headers.items()
 
         payload = {
+            'uid': uid,
             'from': sent_from,
             'to': sent_to,
-            'cc': None,
-            'bcc': None,
-            'subject': subject,
             'headers': headers,
+            'date': date,
+            'subject': subject,
+            'message_id': message_id,
             'body': body,
             'attachments': None
         }
 
         self._sensor_service.dispatch(trigger=self._trigger, payload=payload)
-
