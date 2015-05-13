75fcb95c04bc56729a1521177ac0e2cb4462bbba
Tomaz Muraus <tomaz@tomaz.me>
2015-05-13 18:58:39 +0200

    Add support for nested attribute lookup and formatting.

--- packs/st2/actions/chatops_format_list_result.py
+++ packs/st2/actions/chatops_format_list_result.py
@@ -17,7 +17,7 @@ class St2ChatOpsFormatListResult(Action):
         # Add headers
         header = []
         for attribute in attributes:
-            name = attribute.title()
+            name = self._get_header_attribute_name(attribute=attribute)
             header.append(name)
         table.field_names = header
 
@@ -25,10 +25,34 @@ class St2ChatOpsFormatListResult(Action):
         for item in result:
             row = []
             for attribute in attributes:
-                value = item.get(attribute, None)
+                value = self._get_attribute_value(attribute=attribute, item=item)
                 row.append(value)
 
             table.add_row(row)
 
         result = table.get_string()
         return result
+
+    def _get_header_attribute_name(self, attribute):
+        name = attribute.replace('_', ' ').replace('.', ' ').title()
+        return name
+
+    def _get_attribute_value(self, attribute, item):
+        if '.' in attribute:
+            value = self._get_complex_attribute_value(attribute=attribute, item=item)
+        else:
+            value = item.get(attribute, None)
+
+        return value
+
+    def _get_complex_attribute_value(self, attribute, item):
+        attribute_names = attribute.split('.')
+
+        for index in range(0, (len(attribute_names) - 1)):
+            attribute_name = attribute_names[index]
+            item = item.get(attribute_name, {})
+
+        attribute_name = attribute_names[-1]
+        value = item.get(attribute_name, None)
+
+        return value
