666a5d1d73bc32c14822075082a7413f2cb013fc
James Fryman <james@fryman.io>
2015-05-13 15:35:45 -0500

    WIP SMTP Sensor

--- packs/email/sensors/smtp_sensor.py
+++ packs/email/sensors/smtp_sensor.py
@@ -1,17 +1,9 @@
-#       - from
-#       - to
-#       - cc
-#       - bcc
-#       - headers
-#       - subject
-#       - body
-#       - attachments
 import eventlet
-import asyncore
 import smtpd
+from eventlet.green import asyncore
 from flanker import mime
 
-from st2reactor.sensor.base import PollingSensor
+from st2reactor.sensor.base import Sensor
 
 __all__ = [
     'SMTPSensor'
@@ -24,27 +16,31 @@ eventlet.monkey_patch(
     thread=True,
     time=True)
 
-class SMTPSensor(PollingSensor):
-    def __init__(self, sensor_service, config=None, poll_interval=1):
+class SMTPSensor(Sensor):
+    def __init__(self, sensor_service, config=None):
         super(SMTPSensor, self).__init__(sensor_service=sensor_service,
-                                         config=config,
-					 poll_interval=poll_interval)
+                                         config=config)
 
-        self._trigger = 'email.message'
+        self._trigger = 'email.smtp.message'
         self._logger = self._sensor_service.get_logger(__name__)
+        self._server = None
+        self._listen_ip = self._config.get('smtp_listen_ip', '127.0.0.1')
+        self._listen_port = self._config.get('smtp_listen_port', 25)
 
     def setup(self):
-        self._server = St2SMTPServer(localaddr=('127.0.0.1', 25),
+        self._logger.debug('[SMTPSensor]: entering setup')
+        self._server = St2SMTPServer(localaddr=(self._listen_ip, self._listen_port),
                                      remoteaddr=None,
                                      sensor_service=self._sensor_service,
                                      logger=self._logger,
                                      trigger=self._trigger)
 
-    def poll(self):
+    def run(self):
+        self._logger.debug('[SMTPSensor]: entering run')
         asyncore.loop()
 
     def cleanup(self):
-        pass
+        self._logger.debug('[SMTPSensor]: entering cleanup')
 
     def add_trigger(self, trigger):
         pass
@@ -64,6 +60,7 @@ class St2SMTPServer(smtpd.SMTPServer):
 
     def process_message(self, peer, mailfrom, rcpttos, data):
         self._logger.debug('posting message')
+
         message = mime.from_string(data)
         subject = message.subject()
         headers = json.dumps(message.header.items())
