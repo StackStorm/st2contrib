4d523188dca6e85e858a7a813261d56e7b95e8d4
Tomaz Muraus <tomaz@tomaz.me>
2015-05-13 18:22:11 +0200

    Add ChatOps stuff for sensors-list command.

--- packs/st2/actions/sensors_list.py
+++ packs/st2/actions/sensors_list.py
@@ -1,10 +1,16 @@
 from lib.action import St2BaseAction
-from lib.utils import filter_none_values
 from lib.formatters import format_client_list_result
 
 __all__ = [
     'St2SensorsListAction'
 ]
+
+EXCLUDE_ATTRIBUTES = [
+]
+
+
+def format_result(result):
+    return format_client_list_result(result=result, exclude_attributes=EXCLUDE_ATTRIBUTES)
 
 
 class St2SensorsListAction(St2BaseAction):
@@ -14,10 +20,7 @@ class St2SensorsListAction(St2BaseAction):
         if pack:
             kwargs['pack'] = pack
 
-        # Filter out parameters with string value of "None"
-        # This is a work around since the default values can only be strings
-        kwargs = filter_none_values(kwargs)
         result = self._run_client_method(method=self.client.sensors.get_all,
                                          method_kwargs=kwargs,
-                                         format_func=format_client_list_result)
+                                         format_func=format_result)
         return result
