dcd07663d5aa4acde8143fa21d7b0e096edd775b
manasdk <manasdk@gmail.com>
2015-05-14 16:32:30 -0700

    Use screen_name instead of user
    * screen_name is an unambigious property

--- packs/twitter/actions/direct_message.py
+++ packs/twitter/actions/direct_message.py
@@ -9,7 +9,7 @@ __all__ = [
 
 
 class DirectMessageAction(Action):
-    def run(self, user, message):
+    def run(self, screen_name, message):
         auth = OAuth(
             token=self.config['access_token'],
             token_secret=self.config['access_token_secret'],
@@ -17,6 +17,6 @@ class DirectMessageAction(Action):
             consumer_secret=self.config['consumer_secret']
         )
         client = Twitter(auth=auth)
-        client.direct_messages.new(user=user, text=message)
+        client.direct_messages.new(user=screen_name, text=message)
 
         return True
