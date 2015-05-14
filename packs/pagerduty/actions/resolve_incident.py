306d9e7c2c2553828fd1228a3b0d21fbfc0ccd3a
Tomaz Muraus <tomaz@tomaz.me>
2015-05-14 11:26:15 +0200

    Fix pagerduty actions which reference an invalid variable.

--- packs/pagerduty/actions/resolve_incident.py
+++ packs/pagerduty/actions/resolve_incident.py
@@ -2,9 +2,9 @@ from lib.action import PagerDutyAction
 
 
 class ResolveIncident(PagerDutyAction):
-  def run(self, event_keys):
-    """Resolve an incident, You can provide comma(,) separated keys for resolving more events at once"""
-    for arg in event_keys:
-      self.pager.resolve_incident(self.config['service_api'], arg)
-    return resolve_keys
+    def run(self, event_keys):
+        """Resolve an incident, You can provide comma(,) separated keys for resolving more events at once"""
+        for arg in event_keys:
+            self.pager.resolve_incident(self.config['service_api'], arg)
 
+        return event_keys
