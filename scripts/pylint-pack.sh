16652b5c87768bbba0968615874dc7eb28beae55
Tomaz Muraus <tomaz@tomaz.me>
2015-05-15 19:00:28 +0200

    Update script which runs pylint on a pack to also install pack Python requirements if there are any.
    This way pylint also knows about the structured about the referenced dependency
    classes and modules.

--- scripts/pylint-pack.sh
+++ scripts/pylint-pack.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+VIRTUALENV_DIR=virtualenv
+
 PACK_PATH=$1
 PACK_NAME=$(basename ${PACK_PATH})
 
 function join { local IFS="$1"; shift; echo "$*"; }
 
 COMPONENTS=$(find /tmp/st2/* -maxdepth 1 -name "st2*" -type d)
-PYTHONPATH=$(join ":" ${COMPONENTS})
+PACK_PYTHONPATH=$(join ":" ${COMPONENTS})
 
 echo "Running pylint on pack: ${PACK_NAME}"
 
@@ -15,6 +17,32 @@ if [ ! -d "${PACK_PATH}/actions" -a ! -d "${PACK_PATH}/sensors" -a ! -d "${PACK_
     exit 0
 fi
 
+
+# We create virtualenv and instasll all the pack dependencies. This way pylint can also
+# correctly instrospect all the dependency references
+PACK_VIRTUALENV_DIR="/tmp/venv-${PACK_NAME}"
+PACK_REQUIREMENTS_FILE="${PACK_PATH}/requirements.txt"
+PYTHON_BINARY=${PACK_VIRTUALENV_DIR}/bin/python
+
+# Create virtualenv
+if [ ! -d "${PACK_VIRTUALENV_DIR}" ]; then
+    virtualenv --no-site-packages ${PACK_VIRTUALENV_DIR}
+
+    # Install base dependencies
+    ${PACK_VIRTUALENV_DIR}/bin/pip install -r requirements-dev.txt
+fi
+
+# Install per-pack dependencies
+if [ -f "${PACK_REQUIREMENTS_FILE}" ]; then
+    echo "Installing pack requirements.txt into ${PACK_VIRTUALENV_DIR}"
+
+    # Install dependencies
+    ${PACK_VIRTUALENV_DIR}/bin/pip install -r ${PACK_REQUIREMENTS_FILE}
+    PYTHON_BINARY=${PACK_VIRTUALENV_DIR}/bin/python
+fi
+
+export PYTHONPATH=${PACK_PYTHONPATH}:${PYTHONPATH}
+
 #echo "PYTHONPATH=${PYTHONPATH}"
-export PYTHONPATH=${PYTHONPATH}
-find ${PACK_PATH}/* -name "*.py" -print0 | xargs -0 pylint -E --rcfile=./.pylintrc
+#echo "PYTHON_BINARY=${PYTHON_BINARY}"
+find ${PACK_PATH}/* -name "*.py" -print0 | xargs -0 ${PYTHON_BINARY} -m pylint -E --rcfile=./.pylintrc
