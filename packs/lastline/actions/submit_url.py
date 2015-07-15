df242c0ded35845f8030ac2c01813f634472e175
James Fryman <james@fryman.io>
2015-07-15 11:09:17 -0500

    revert formatting change

--- packs/lastline/actions/submit_url.py
+++ packs/lastline/actions/submit_url.py
@@ -6,6 +6,10 @@ class SubmitURL(actions.BaseAction):
             backend=None, analysis_timeout=None, push_to_portal_account=None,
             raw=False, verify=True, user_agent=None, report_version=None):
 
-        response = self.client.submit_url(url, referer, full_report_score, bypass_cache, backend, analysis_timeout, push_to_portal_account, raw, verify, user_agent, report_version)
+        response = self.client.submit_url(url, referer, full_report_score,
+                                          bypass_cache, backend,
+                                          analysis_timeout,
+                                          push_to_portal_account, raw, verify,
+                                          user_agent, report_version)
 
         return response
