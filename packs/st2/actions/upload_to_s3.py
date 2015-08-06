1161458aa3a0b21c3cef9a1eae5cf4f017662750
James Fryman <james@fryman.io>
2015-08-06 17:03:18 -0500

    Cleaninup up wf

--- packs/st2/actions/upload_to_s3.py
+++ packs/st2/actions/upload_to_s3.py
@@ -7,11 +7,10 @@ __all__ = [
 
 
 class UploadToS3(St2BaseAction):
-    def run(self, uuid, file_name, bucket, data_type):
+    def run(self, file_name, remote_file, bucket):
         try:
             connection = s3.S3Connection(**self.config['s3'])
             storage = s3.Storage(connection)
-            remote_file = "{}-{}".format(uuid, data_type)
             remote_name = s3.S3Name(remote_file, bucket=bucket)
             storage.write(file_name, remote_name)
             exists, metadata = storage.exists(remote_name)
