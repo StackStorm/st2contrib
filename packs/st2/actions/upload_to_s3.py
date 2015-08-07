056dd793bd931c2aeed09fcb62fb8ece3df7ba61
James Fryman <james@fryman.io>
2015-08-06 21:56:42 -0500

    adding python action

--- packs/st2/actions/upload_to_s3.py
+++ packs/st2/actions/upload_to_s3.py
@@ -8,18 +8,14 @@ __all__ = [
 
 class UploadToS3(St2BaseAction):
     def run(self, file_name, remote_file, bucket):
-        try:
-            connection = s3.S3Connection(**self.config['s3'])
-            storage = s3.Storage(connection)
-            remote_name = s3.S3Name(remote_file, bucket=bucket)
-            storage.write(file_name, remote_name)
-            exists, metadata = storage.exists(remote_name)
-            assert exists
-            payload = {
-                "status": "ok",
-                "uploaded_file": remote_file,
-            }
+        remote_name = s3.S3Name(remote_file, bucket=bucket)
 
-            return payload
-        except StorageError, e:
-            raise Exception('S3 upload failed: {}'.format(e))
+        connection = s3.S3Connection(**self.config['s3'])
+        storage = s3.Storage(connection)
+        storage.write(file_name, remote_name)
+        payload = {
+            "status": "ok",
+            "uploaded_file": remote_file,
+        }
+
+        return payload
