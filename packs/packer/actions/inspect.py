4f219e4f59a75f5678fb0daa828b25f5a44b1f2d
James Fryman <james@fryman.io>
2015-08-20 12:07:27 -0500

    proper merge and indentation

--- packs/packer/actions/inspect.py
+++ packs/packer/actions/inspect.py
@@ -5,7 +5,6 @@ class InspectAction(BaseAction):
     def run(self, packerfile, cwd=None):
         if cwd:
             self.set_dir(cwd)
-
         p = self.packer(packerfile)
         result = p.inspect(mrf=True)
         return result.parsed_output
--- packs/packer/actions/lib/actions.py
+++ packs/packer/actions/lib/actions.py
@@ -23,7 +23,7 @@ class BaseAction(Action):
         for k in set(dict1.keys()).union(dict2.keys()):
             if k in dict1 and k in dict2:
                 if isinstance(dict1[k], dict) and isinstance(dict2[k], dict):
-                    yield (k, dict(mergedicts(dict1[k], dict2[k])))
+                    yield (k, dict(self._mergedicts(dict1[k], dict2[k])))
                 else:
                     yield (k, dict2[k])
             elif k in dict1:
