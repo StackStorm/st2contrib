19284adfe95ae9fc09b1fc522374198c9094452d
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-09-02 08:29:11 +1000

    Added optional parameter for creating a balancer (the algorithm)

--- packs/libcloud/actions/create_balancer.py
+++ packs/libcloud/actions/create_balancer.py
@@ -14,12 +14,25 @@ RECORD_ATTRIBUTES = [
 class CreateBalancerAction(BaseAction):
     api_type = 'loadbalancer'
 
-    def run(self, credentials, name, port, protocol):
+    def run(self, credentials, name, port, protocol,
+            algorithm=Algorithm.ROUND_ROBIN):
         driver = self._get_driver_for_credentials(credentials=credentials)
+
+        # Use a local lookup - these maps are protected fields of each driver
+        _VALUE_TO_ALGORITHM_MAP = {
+            'ROUND_ROBIN': Algorithm.ROUND_ROBIN,
+            'LEAST_CONNECTIONS': Algorithm.LEAST_CONNECTIONS,
+            'SHORTEST_RESPONSE': Algorithm.SHORTEST_RESPONSE,
+            'PERSISTENT_IP': Algorithm.PERSISTENT_IP
+        }
+
+        if algorithm is not Algorithm.ROUND_ROBIN:
+            algorithm = _VALUE_TO_ALGORITHM_MAP[algorithm]
+
         record = driver.create_balancer(name=name,
                                         port=port,
                                         protocol=protocol,
-                                        algorithm=Algorithm.ROUND_ROBIN,
+                                        algorithm=algorithm,
                                         members=None)
 
         result = []
