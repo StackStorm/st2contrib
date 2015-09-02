4d53498683a7fe344700724b22a0c2d75a2ed10a
armab <dev@arma.biz>
2015-09-02 21:02:12 +0100

    [trello] Minor updates
    - add 'description' param to 'trello.add_card' action

--- packs/trello/actions/add_card.py
+++ packs/trello/actions/add_card.py
@@ -2,13 +2,13 @@ from lib import action
 
 
 class AddCardAction(action.BaseAction):
-    def run(self, name, board_id, list_id, api_key=None, token=None):
+    def run(self, name, board_id, list_id, description=None, api_key=None, token=None):
         if api_key:
             self._set_creds(api_key=api_key, token=token)
 
         board = self._client().get_board(board_id)
         lst = board.get_list(list_id)
-        card = lst.add_card(name)
+        card = lst.add_card(name=name, desc=description)
 
         if card:
             return card.id
