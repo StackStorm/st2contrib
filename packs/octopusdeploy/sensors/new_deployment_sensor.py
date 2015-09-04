6ce94f0975edd5b905c6b814da4cbf677fe164a3
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-09-04 14:19:49 +1000

    Added a sensor for new deployments as well

--- packs/octopusdeploy/sensors/new_deployment_sensor.py
+++ packs/octopusdeploy/sensors/new_deployment_sensor.py
@@ -3,7 +3,7 @@ import time
 from lib.sensors import OctopusDeploySensor
 
 
-class NewReleaseSensor(OctopusDeploySensor):
+class NewDeploymentSensor(OctopusDeploySensor):
     """
     * self._sensor_service
         - provides utilities like
@@ -17,32 +17,32 @@ class NewReleaseSensor(OctopusDeploySensor):
     """
 
     def __init__(self, sensor_service, config=None, poll_interval=None):
-        super(NewReleaseSensor, self).__init__(sensor_service=sensor_service,
-                                               config=config,
-                                               poll_interval=poll_interval)
-        self._trigger_ref = 'octopusdeploy.new_release'
+        super(NewDeploymentSensor, self).__init__(sensor_service=sensor_service,
+                                                  config=config,
+                                                  poll_interval=poll_interval)
+        self._trigger_ref = 'octopusdeploy.new_deployment'
         self._logger = self._sensor_service.get_logger(__name__)
-        self._release_key = 'octopusdeploy.last_release_date_str'
+        self._deploy_key = 'octopusdeploy.last_deploy_date_str'
         self._last_date = None
 
     def setup(self):
         pass
 
     def poll(self):
-        self._logger.debug('Requesting list of releases')
-        releases = self._get_releases()
+        self._logger.debug('Requesting list of deployments')
+        deployments = self._get_deployments()
 
         # Make sure there are releases
-        if releases is None:
-            self._logger.info('No releases found')
+        if deployments is None:
+            self._logger.info('No deployments found')
             return
-        if len(releases) is 0:
-            self._logger.info('Empty list of releases')
+        if len(deployments) is 0:
+            self._logger.info('Empty list of deployments')
             return
 
-        last_release = releases[0]
+        last_deployment = deployments[0]
 
-        last_assembled_date = self._to_date(last_release['assembled'])
+        last_assembled_date = self._to_date(last_deployment['assembled'])
 
         # What is the last indexed release date? If none, index and exit
         index_date = self._get_last_date()
@@ -52,21 +52,21 @@ class NewReleaseSensor(OctopusDeploySensor):
             self._set_last_date(last_assembled_date)
             index_date = self._get_last_date()
 
-        # If there have been new releases, trigger them each
+        # If there have been new deployments, trigger them each
         if last_assembled_date > index_date:
-            self._logger.info('Found releases to trigger')
-            # Get releases since the last update time
+            self._logger.info('Found deployments to trigger')
+            # Get deployments since the last update time
             # They are in date order so once you get one behind the index
             # break out of the loop
-            for release in releases:
-                if self._to_date(release['assembled']) > index_date:
-                    self._logger.info('Raising trigger for %s' % release['id'])
-                    self._dispatch_trigger_for_release(release)
+            for deployment in deployments:
+                if self._to_date(deployment['assembled']) > index_date:
+                    self._logger.info('Raising trigger for %s' % deployment['id'])
+                    self._dispatch_trigger_for_deployment(deployment)
                 else:
                     break
             self._set_last_date(last_assembled_date)
         else:
-            self._logger.debug('No new releases comparing %s'
+            self._logger.debug('No new deployments comparing %s'
                                % time.strftime("%Y-%m-%dT%H:%M:%S",
                                                last_assembled_date))
             self._logger.debug('and %s'
@@ -85,8 +85,8 @@ class NewReleaseSensor(OctopusDeploySensor):
     def remove_trigger(self, trigger):
         pass
 
-    def _get_releases(self):
-        result = self.make_get_request("releases")
+    def _get_deployments(self):
+        result = self.make_get_request("deployments")
         releases = self._to_triggers(result['Items'])
         return releases
 
@@ -94,23 +94,23 @@ class NewReleaseSensor(OctopusDeploySensor):
         date_string = date_string.split('.')[0]
         return time.strptime(date_string, '%Y-%m-%dT%H:%M:%S')
 
-    def _get_last_release(self):
-        releases = self._get_releases()
-        if releases is None:
+    def _get_last_deployment(self):
+        deployments = self._get_deployments()
+        if deployments is None:
             return None
-        if len(releases) is not 1:
+        if len(deployments) is not 1:
             return None
-        return releases[0]
+        return deployments[0]
 
     def _get_last_date(self):
-        self._last_date = self._sensor_service.get_value(name=self._release_key)
+        self._last_date = self._sensor_service.get_value(name=self._deploy_key)
         if self._last_date is None:
             return None
         return time.strptime(self._last_date, '%Y-%m-%dT%H:%M:%S')
 
     def _set_last_date(self, last_date):
         self._last_date = time.strftime('%Y-%m-%dT%H:%M:%S', last_date)
-        self._sensor_service.set_value(name=self._release_key,
+        self._sensor_service.set_value(name=self._deploy_key,
                                        value=self._last_date)
 
     def _to_triggers(self, releases):
@@ -119,17 +119,18 @@ class NewReleaseSensor(OctopusDeploySensor):
             triggers.append(self._to_trigger(release))
         return triggers
 
-    def _to_trigger(self, release):
+    def _to_trigger(self, deployment):
         trigger = {
-            'id': release['Id'],
-            'assembled': release['Assembled'],
-            'release_notes': release['ReleaseNotes'],
-            'version': release['Version'],
-            'author': release['LastModifiedBy'],
-            'project_id': release['ProjectId']
+            'id': deployment['Id'],
+            'name': deployment['Name'],
+            'comments': deployment['Comments'],
+            'assembled': deployment['Created'],
+            'version': deployment['ReleaseId'],
+            'author': deployment['LastModifiedBy'],
+            'project_id': deployment['ProjectId']
         }
         return trigger
 
-    def _dispatch_trigger_for_release(self, trigger_payload):
+    def _dispatch_trigger_for_deployment(self, trigger_payload):
         trigger = self._trigger_ref
         self._sensor_service.dispatch(trigger=trigger, payload=trigger_payload)
