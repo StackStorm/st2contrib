c5cff005d75fdc6eb5533d11b13411a3cb5d7bf8
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-09-10 14:33:13 +1000

    fix alignment

--- packs/octopusdeploy/actions/deploy_release.py
+++ packs/octopusdeploy/actions/deploy_release.py
@@ -8,7 +8,6 @@ __all__ = [
 class DeployReleaseAction(OctopusDeployAction):
     def run(self, release_id, environment_id):
         result = self.make_post_request(action="deployments",
-                                        payload=
-                                        {"ReleaseId": release_id,
-                                         "EnvironmentId": environment_id})
+                                        payload={"ReleaseId": release_id,
+                                                 "EnvironmentId": environment_id})
         return result
