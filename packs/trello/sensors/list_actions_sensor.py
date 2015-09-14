3a3752f2796eeac5c421bb42c4b6a53ef7faed01
armab <dev@arma.biz>
2015-09-14 23:40:02 +0100

    Cosmetic fixes

--- packs/trello/sensors/list_actions_sensor.py
+++ packs/trello/sensors/list_actions_sensor.py
@@ -20,16 +20,16 @@ class TrelloListSensor(PollingSensor):
         super(TrelloListSensor, self).__init__(sensor_service, config, poll_interval)
         self._logger = self._sensor_service.get_logger(__name__)
 
-        list_sensor = self._config.get('list_sensor')
-        if not list_sensor:
+        list_actions_sensor = self._config.get('list_actions_sensor')
+        if not list_actions_sensor:
             raise ValueError('[TrelloListSensor]: "list_sensor" config value is required!')
 
-        self._board_id = list_sensor.get('board_id')
+        self._board_id = list_actions_sensor.get('board_id')
         if not self._board_id:
             raise ValueError('[TrelloListSensor]: "list_sensor.board_id" config value is required!')
         assert isinstance(self._board_id, basestring)
 
-        self._list_id = list_sensor.get('list_id')
+        self._list_id = list_actions_sensor.get('list_id')
         if not self._list_id:
             raise ValueError('[TrelloListSensor]: "list_sensor.list_id" config value is required!')
         assert isinstance(self._list_id, basestring)
@@ -61,8 +61,8 @@ class TrelloListSensor(PollingSensor):
         """
         _list = self._client.get_board(self._board_id).get_list(self._list_id)
         monkey_patch_trello(_list)
-        _list.fetch_actions(
-            action_filter=self._config['list_sensor'].get('filter') or None,
+        _list._fetch_actions(
+            action_filter=self._config['list_actions_sensor'].get('filter') or None,
             filter_since=self._sensor_service.get_value(self.key_name)
         )
 
@@ -98,7 +98,7 @@ class TrelloListSensor(PollingSensor):
 
 def monkey_patch_trello(trello_list):
     """
-    Function to overwrite existing method in Trello List object.
+    Function to add method to existing Trello List object.
 
     :param trello_list: Input Trello List object to monkey-patch.
     :type trello_list: :class:`trello.List`
@@ -122,14 +122,15 @@ def monkey_patch_trello(trello_list):
         """
         json_obj = self.client.fetch_json(
             '/lists/' + self.id + '/actions',
-            query_params = {
+            query_params={
                 'filter': action_filter,
                 'since': filter_since,
             })
         self.actions = json_obj
         return self.actions
 
-    trello_list.fetch_actions = MethodType(_fetch_actions, trello_list)
+    trello_list._fetch_actions = MethodType(_fetch_actions, trello_list)
+
 
 def is_date(string):
     """
