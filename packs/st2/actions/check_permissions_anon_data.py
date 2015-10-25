637a064b93fecc57b48a17ecdbd5af78906cd085
manasdk <manas@stackstorm.com>
2015-10-25 10:55:20 -0700

    Rework the collection and test of anon_data property
    * Since the key name is st2::collect_anonymous_data the inclusion of
      `::` throws off jinja and this cannot be used as the previous approach.
    * New action-chain task to get the property and string based comparison
      to match the property.

--- packs/st2/actions/check_permissions_anon_data.py
+++ packs/st2/actions/check_permissions_anon_data.py
@@ -5,7 +5,7 @@ from st2actions.runners.pythonrunner import Action
 
 class CheckPermission(Action):
     def run(self, collect_anonymous_data):
-        if collect_anonymous_data is True:
+        if collect_anonymous_data.lower() == "true":
             return True
         # No permission therefore exit 1
         sys.exit(1)
