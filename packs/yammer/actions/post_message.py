47d9e664c43a6c1f935cd6cf9ddf9d26be31e92b
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-11-19 06:52:56 +0800

    Added missing arg

--- packs/yammer/actions/post_message.py
+++ packs/yammer/actions/post_message.py
@@ -6,8 +6,9 @@ __all__ = [
 
 
 class PostMessageAction(YammerAction):
-    def run(self, message, group_id, topics, replied_to_id):
+    def run(self, message, group_id, topics, replied_to_id, direct_to_id):
         yammer = self.authenticate()
         return yammer.messages.create(
             message, group_id=group_id,
-            topics=topics, replied_to_id=replied_to_id)
+            topics=topics, replied_to_id=replied_to_id,
+            direct_to_id=direct_to_id)
