79bfccf7e765481ea7f47fb3355e4ee3ca8cc1a0
Tomaz Muraus <tomaz@tomaz.me>
2015-12-14 13:08:45 +0100

    Run "packs-register-resources" task on Travis.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -14,6 +14,8 @@ elif [ ${TASK} == "configs-check" ]; then
   make configs-check
 elif [ ${TASK} == "metadata-check" ]; then
   make metadata-check
+elif [ ${TASK} == "pack-register-resources" ]; then
+  make packs-register-resources
 elif [ ${TASK} == "packs-tests" ]; then
   make packs-tests
 else
