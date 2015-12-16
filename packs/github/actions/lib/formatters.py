8b0dcf1bfda26ab9463d2c5a892b7ffd3fa015d9
Tomaz Muraus <tomaz@tomaz.me>
2015-12-16 12:29:05 +0100

    Make sure we flatten the labels attribute to a serializable simple type.

--- packs/github/actions/lib/formatters.py
+++ packs/github/actions/lib/formatters.py
@@ -1,5 +1,6 @@
 __all__ = [
-    'issue_to_dict'
+    'issue_to_dict',
+    'label_to_dict'
 ]
 
 
@@ -17,8 +18,24 @@ def issue_to_dict(issue):
     result['body'] = issue.body
     result['url'] = issue.html_url
     result['state'] = issue.state
-    result['labels'] = issue.labels
+
+    if issue.labels:
+        labels = [label_to_dict(label) for label in issue.labels]
+    else:
+        labels = []
+
+    result['labels'] = labels
     result['created_at'] = issue.created_at
     result['closed_at'] = issue.closed_at
     result['closed_by'] = closed_by
+    return result
+
+
+def label_to_dict(label):
+    result = {}
+
+    result['name'] = label.name
+    result['color'] = label.color
+    result['url'] = label.url
+
     return result
