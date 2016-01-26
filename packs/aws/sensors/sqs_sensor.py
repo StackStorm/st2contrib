ff8044f353edf51ba47dc777bc444727e7a12e45
Adam Hamsik <adh@rsd.com>
2016-01-26 16:49:28 +0100

    Add ability to go through multiple queues.

--- packs/aws/sensors/sqs_sensor.py
+++ packs/aws/sensors/sqs_sensor.py
@@ -4,7 +4,7 @@ After receiving a message it's content is passed as payload to a trigger 'aws.sq
 
 This sensor can be configured either by using config.yaml withing a pack or by creating
 following values in datastore:
-    - aws.input_queue
+    - aws.input_queues (list queues as comma separrated string: first_queue,second_queue)
     - aws.aws_access_key_id
     - aws.aws_secret_access_key
     - aws.region
@@ -15,7 +15,9 @@ For configuration in config.yaml with config like this
       aws_access_key_id:
       region:
     sqs_sensor:
-      input_queue:
+      input_queues:
+        - first_queue
+        - second_queue
 
 If any value exist in datastore it will be taken instead of any value in config.yaml
 """
@@ -32,9 +34,17 @@ class AWSSQSSensor(PollingSensor):
                                            poll_interval=poll_interval)
 
     def setup(self):
-        self.input_queue = self._get_config_entry(key='keys ', prefix='sqs_sensor')
+        queues = self._get_config_entry(key='input_queues', prefix='sqs_sensor')
+
+        # XXX: This is a hack as from datastore we can only receive a string while
+        # from config.yaml we can receive a list
+        if type(queues) is str:
+            self.input_queues = queues.split(', ')
+        else:
+            self.input_queues = queues
+
         self.aws_access_key = self._get_config_entry('aws_access_key_id')
-        self.aws_secret_key = self._get_config_entry('aws_access_key_id')
+        self.aws_secret_key = self._get_config_entry('aws_secret_access_key')
         self.aws_region = self._get_config_entry('region')
 
         self._logger = self._sensor_service.get_logger(name=self.__class__.__name__)
@@ -43,14 +53,14 @@ class AWSSQSSensor(PollingSensor):
         self.sqs_res = None
 
         self._setup_sqs()
-        self.queue = self._get_queue_by_name(self.input_queue)
 
     def poll(self):
-        msg = self._receive_messages(queue=self.queue)
-        if msg:
-            payload = {"queue": self.input_queue, "body": msg[0].body}
-            self._sensor_service.dispatch(trigger="aws.sqs_new_message", payload=payload)
-            msg[0].delete()
+        for queue in self.input_queues:
+            msg = self._receive_messages(queue=self._get_queue_by_name(queue))
+            if msg:
+                payload = {"queue": queue, "body": msg[0].body}
+                self._sensor_service.dispatch(trigger="aws.sqs_new_message", payload=payload)
+                msg[0].delete()
 
     def cleanup(self):
         pass
@@ -73,6 +83,9 @@ class AWSSQSSensor(PollingSensor):
         value = self._sensor_service.get_value('aws.%s' % (key), local=False)
         if not value:
             value = config.get(key, None)
+
+        if not value:
+            raise ValueError('[AWSSQSSensor]: Configuration for %s key is missing.' % (key))
 
         return value
 
