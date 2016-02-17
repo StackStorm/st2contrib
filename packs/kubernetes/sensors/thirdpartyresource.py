20a471cde2b205fc6df90696d6fa2296a2714835
Ward, Michael S <Michael.Ward@pearson.com>
2016-02-16 21:47:11 -0700

    made changes to make thirdpartyresource.py more testable

--- packs/kubernetes/sensors/thirdpartyresource.py
+++ packs/kubernetes/sensors/thirdpartyresource.py
@@ -26,35 +26,49 @@ class ThirdPartyResource(Sensor):
         r = self.client
         lines = r.iter_lines()
         # Save the first line for later or just skip it
-#        first_line = next(lines)
-        for line in lines:
-            io = json.dumps(line)
-            n = json.loads(io)
-            d_list = ast.literal_eval(n)
-            self._k8s_object(d_list=d_list)
-#            self._resource_version(d_list=d_list)
+        # first_line = next(lines)
 
-    def _k8s_object(self, d_list):
+        trigger = 'kubernetes.thirdpartyobject'
+
+        for line in lines:
+            try:
+                trigger_payload = self._get_trigger_payload_from_line(line)
+            except:
+                sys.exit(1)
+            else:
+                self._sensor_service.dispatch(trigger=trigger, payload=trigger_payload)
+
+    def _get_trigger_payload_from_line(self, line):
+        # need to perform a json dump due to uft8 error prior to performing a json.load
+        io = json.dumps(line)
+        n = json.loads(io)
+        line = ast.literal_eval(n)
+        payload = self._k8s_object(line)
+        return payload
+
+    def _k8s_object(self, line):
         # Define some variables
         try:
-            resource_type = d_list['type']
-            object_kind = d_list['object']['kind']
-            name = d_list['object']['metadata']['name']
-            namespace = d_list['object']['metadata']['namespace']
-            uid = d_list['object']['metadata']['uid']
+            resource_type = line['type']
+            object_kind = line['object']['kind']
+            name = line['object']['metadata']['name']
+            namespace = line['object']['metadata']['namespace']
+            uid = line['object']['metadata']['uid']
         except:
             self._log.debug("type, kind, name, namespace or uid do not exist in the object.\
                               must exit")
             sys.exit()
-        # Now lets see if labels exist, if so build a trigger
-        if 'labels' in d_list['object']['metadata']:
-            labels_data = d_list['object']['metadata']['labels']
+
+        # Now lets see if labels exist, if so build a trigger else exit
+        if 'labels' in line['object']['metadata']:
+            labels_data = line['object']['metadata']['labels']
             self._build_a_trigger(resource_type=resource_type, name=name, labels=labels_data,
                                   namespace=namespace, object_kind=object_kind, uid=uid)
         else:
             self._log.debug("No Labels for the resource below. Tough to proceed without knowing how \
                   to work with this object.")
             self._log.debug(name, namespace, uid)
+            sys.exit(1)
 
     def _build_a_trigger(self, resource_type, name, labels, namespace, object_kind, uid):
         trigger = 'kubernetes.thirdpartyobject'
