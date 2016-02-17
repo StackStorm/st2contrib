c526823948167d368689d70bd895f210fd0a1d3a
Lakshmi Kannan <lakshmi+github@lakshmikannan.me>
2016-02-17 14:49:49 -0800

    Add some unit tests

--- packs/kubernetes/sensors/third_party_resource.py
+++ packs/kubernetes/sensors/third_party_resource.py
@@ -10,13 +10,23 @@ from st2reactor.sensor.base import Sensor
 
 
 class ThirdPartyResource(Sensor):
-    def setup(self):
+    def __init__(self, sensor_service, config=None):
+        super(ThirdPartyResource, self).__init__(sensor_service=sensor_service,
+                                                 config=config)
         self._log = self._sensor_service.get_logger(__name__)
-        extension = self._config['extension_url']
-        KUBERNETES_API_URL = self._config['kubernetes_api_url'] + extension
-        user = self._config['user']
-        password = self._config['password']
-        verify = self._config['verify']
+        self.TRIGGER_REF = 'kubernetes.thirdpartyobject'
+        self.client = None
+
+    def setup(self):
+        try:
+            extension = self._config['extension_url']
+            KUBERNETES_API_URL = self._config['kubernetes_api_url'] + extension
+            user = self._config['user']
+            password = self._config['password']
+            verify = self._config['verify']
+        except KeyError:
+            self._log.exception('Configuration file does not contain required fields.')
+            raise
         self._log.debug('Connecting to Kubernetes endpoint %s via api_client.' %
                         KUBERNETES_API_URL)
         self.client = requests.get(KUBERNETES_API_URL, auth=HTTPBasicAuth(user, password),
@@ -29,8 +39,6 @@ class ThirdPartyResource(Sensor):
         # Save the first line for later or just skip it
         # first_line = next(lines)
 
-        trigger = 'kubernetes.thirdpartyobject'
-
         for line in lines:
             try:
                 trigger_payload = self._get_trigger_payload_from_line(line)
@@ -41,43 +49,41 @@ class ThirdPartyResource(Sensor):
                 sys.exit(1)
             else:
                 self._log.debug('Triggering Dispatch Now')
-                self._sensor_service.dispatch(trigger=trigger, payload=trigger_payload)
+                self._sensor_service.dispatch(trigger=self.TRIGGER_REF, payload=trigger_payload)
 
     def _get_trigger_payload_from_line(self, line):
+        k8s_object = self._fix_utf8_enconding_and_eval(line)
+        self._log.debug('Incoming k8s object (from API response): %s', k8s_object)
+        payload = self._k8s_object_to_st2_trigger(k8s_object)
+        return payload
+
+    def _fix_utf8_enconding_and_eval(self, line):
         # need to perform a json dump due to uft8 error prior to performing a json.load
         io = json.dumps(line)
         n = json.loads(io)
         line = ast.literal_eval(n)
-        payload = self._k8s_object(line)
-        return payload
+        return line
 
-    def _k8s_object(self, line):
+    def _k8s_object_to_st2_trigger(self, k8s_object):
         # Define some variables
         try:
-            resource_type = line['type']
-            object_kind = line['object']['kind']
-            name = line['object']['metadata']['name']
-            namespace = line['object']['metadata']['namespace']
-            uid = line['object']['metadata']['uid']
+            resource_type = k8s_object['type']
+            object_kind = k8s_object['object']['kind']
+            name = k8s_object['object']['metadata']['name']
+            namespace = k8s_object['object']['metadata']['namespace']
+            uid = k8s_object['object']['metadata']['uid']
+            labels_data = k8s_object['object']['metadata']['labels']
         except KeyError:
-            msg = 'One of "type", "kind", "name", "namespace" or "uid" ' + \
-                  'do not exist in the object.'
+            msg = 'One of "type", "kind", "name", "namespace" or "uid" or "labels" ' + \
+                  'do not exist in the object. Incoming object=%s' % k8s_object
             self._log.exception(msg)
             raise
-
-        # Now lets see if labels exist, if so build a trigger else exit
-        if 'labels' in line['object']['metadata']:
-            labels_data = line['object']['metadata']['labels']
+        else:
             payload = self._build_a_trigger(resource_type=resource_type, name=name,
-                                            labels=labels_data, namespace=namespace,
-                                            object_kind=object_kind, uid=uid)
+                                    labels=labels_data, namespace=namespace,
+                                    object_kind=object_kind, uid=uid)
             self._log.debug('Trigger payload: %s.' % payload)
             return payload
-        else:
-            msg = 'No Labels for the resource below. Tough to proceed without knowing how ' + \
-                  'to work with this object. Incoming object=%s' % line
-            self._log.error(msg)
-            raise Exception(msg)
 
     def _build_a_trigger(self, resource_type, name, labels, namespace, object_kind, uid):
         payload = {
@@ -90,3 +96,15 @@ class ThirdPartyResource(Sensor):
         }
 
         return payload
+
+    def cleanup(self):
+        pass
+
+    def add_trigger(self, trigger):
+        pass
+
+    def update_trigger(self, trigger):
+        pass
+
+    def remove_trigger(self, trigger):
+        pass
