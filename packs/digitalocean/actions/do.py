bd16f2223c4166cfb14963fa52a18d557ce4f63a
Edward Medvedev <edward.medvedev@gmail.com>
2016-02-22 17:55:29 +0600

    Linting

--- packs/digitalocean/actions/do.py
+++ packs/digitalocean/actions/do.py
@@ -4,8 +4,8 @@ from lib import action
 class DigitalOceanManager(action.BaseAction):
 
     def run(self, **kwargs):
-      action = kwargs['action']
-      del kwargs['action']
-      cls = kwargs['cls']
-      del kwargs['cls']
-      return self.do_action(cls, action, **kwargs)
+        action = kwargs['action']
+        del kwargs['action']
+        cls = kwargs['cls']
+        del kwargs['cls']
+        return self.do_action(cls, action, **kwargs)
--- packs/digitalocean/actions/lib/action.py
+++ packs/digitalocean/actions/lib/action.py
@@ -8,6 +8,6 @@ class BaseAction(Action):
         super(BaseAction, self).__init__(config)
         self._token = config['token']
 
-    def do_action(self,cls,action,**kwargs):
-      obj = getattr(digitalocean,cls)(token=self._token)
-      return getattr(obj,action)(**kwargs)
+    def do_action(self, cls, action, **kwargs):
+        obj = getattr(digitalocean, cls)(token=self._token)
+        return getattr(obj, action)(**kwargs)
