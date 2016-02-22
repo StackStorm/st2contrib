4eb43b4f5427acbf3f90c73637804048fcc80307
Edward Medvedev <edward.medvedev@gmail.com>
2016-02-22 18:01:13 +0600

    Linting and transfer

--- freight/sensors/README.md
+++
@@ -1,4 +0,0 @@
-# sensors
-
-The sensors folder contains sensoros. See [Sensors](http://docs.stackstorm.com/sensors.html) for specifics on writing
-sensors and registering TriggerTypes.
--- packs/freight/actions/add_package.py
+++ packs/freight/actions/add_package.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
-import os, sys, glob, json
-import subprocess, re
+import os
+import sys
+import glob
+import json
+import subprocess
+import re
 
 exitcode = 2
 output = {}
@@ -10,24 +14,26 @@ output['error'] = {}
 # Pop off script name
 sys.argv.pop(0)
 # Repo is last entry, get it then pop it
-repos=sys.argv[-1]
+repos = sys.argv[-1]
 sys.argv.pop(-1)
-#Remaining items are our list
-if re.search('\*',sys.argv[0]):
+# Remaining items are our list
+if re.search('\*', sys.argv[0]):
     files = glob.glob(sys.argv[0])
 else:
     files = sys.argv
 
-if re.search(',',repos):
-    repos = split(',',repos)
+if re.search(',', repos):
+    repos = repos.split(',')
     repos = ' '.join(['apt/%s' % item for item in repos])
 else:
     repos = "apt/%s" % repos
 
 for f in files:
-    ## Check to see if the file exists
+    # Check to see if the file exists
     if os.path.isfile(f):
-        call = subprocess.Popen(['freight', 'add', f, repos], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        call = subprocess.Popen(['freight', 'add', f, repos],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
         stdout, stderr = call.communicate()
         output['result'][f] = stdout
         output['error'][f] = stderr
@@ -38,4 +44,3 @@ for f in files:
 
 print json.dumps(output)
 sys.exit(exitcode)
-
