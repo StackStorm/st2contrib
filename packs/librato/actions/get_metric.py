74e8e1b0387cb8466ab2195fd0e4f2e6e2669fcd
Edward Medvedev <edward.medvedev@gmail.com>
2016-02-23 02:08:40 +0600

    API kwargs

--- packs/librato/actions/get_metric.py
+++ packs/librato/actions/get_metric.py
@@ -3,4 +3,4 @@ from lib import action
 
 class LibratoGetMetric(action.LibratoBaseAction):
     def run(self, name, count=1, resolution=1):
-        self.librato.get(name, count, resolution)
+        self.librato.get(name, count=count, resolution=resolution)
