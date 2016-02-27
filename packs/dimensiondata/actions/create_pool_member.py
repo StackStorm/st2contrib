8a975088008ab33e6cb3dc42567cb1ca195563fa
anthony-shaw <anthony.p.shaw@gmail.com>
2016-02-27 15:56:29 +1100

    Fix add pool member code

--- packs/dimensiondata/actions/create_pool_member.py
+++ packs/dimensiondata/actions/create_pool_member.py
@@ -11,6 +11,6 @@ class CreatePoolMemberAction(BaseAction):
     def run(self, region, pool_id, node_id, port):
         driver = self._get_lb_driver(region)
         pool = driver.ex_get_pool(pool_id)
-        node = driver.ex.get_node(node_id)
+        node = driver.ex_get_node(node_id)
         member = driver.ex_create_pool_member(pool, node, port)
         return self.resultsets.formatter(member)
