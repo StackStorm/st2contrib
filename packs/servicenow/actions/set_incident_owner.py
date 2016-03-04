b22064e9c9100d28d9e0d823b4b23f5f67285661
anthony-shaw <anthony.p.shaw@gmail.com>
2016-03-04 17:24:38 +1100

    Various updates to the servicenow pack

---
+++ packs/servicenow/actions/set_incident_owner.py
@@ -0,0 +1,11 @@
+from lib.actions import BaseAction
+
+
+class AssignIncidentToAction(BaseAction):
+    def run(self, user_id, number):
+        s = self.client
+        s.table='incident'
+        res = s.get({'number': number})
+        sys_id = res[0]['sys_id']
+        response = s.update({'assigned_to': user_id}, sys_id) 
+        return response
