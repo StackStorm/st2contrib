947616cfa32ccf268e903d4f23464371872a0262
anthony-shaw <anthony.p.shaw@gmail.com>
2016-03-04 18:00:14 +1100

    Fix PEP8 issues

--- packs/servicenow/actions/approve_change.py
+++ packs/servicenow/actions/approve_change.py
@@ -4,8 +4,8 @@ from lib.actions import BaseAction
 class ApprovalAction(BaseAction):
     def run(self, number):
         s = self.client
-        s.table='change_request'
+        s.table = 'change_request'
         res = s.get({'number': number})
         sys_id = res[0]['sys_id']
-        response = s.update({'approval': 'approved'}, sys_id) 
+        response = s.update({'approval': 'approved'}, sys_id)
         return response
