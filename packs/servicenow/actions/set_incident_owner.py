60f6e2dd281044e95341745888ce714936abd5dd
anthony-shaw <anthony.p.shaw@gmail.com>
2016-03-05 20:10:33 +1100

    Fix another linting bug

--- packs/servicenow/actions/set_incident_owner.py
+++ packs/servicenow/actions/set_incident_owner.py
@@ -4,8 +4,8 @@ from lib.actions import BaseAction
 class AssignIncidentToAction(BaseAction):
     def run(self, user_id, number):
         s = self.client
-        s.table='incident'
+        s.table = 'incident'
         res = s.get({'number': number})
         sys_id = res[0]['sys_id']
-        response = s.update({'assigned_to': user_id}, sys_id) 
+        response = s.update({'assigned_to': user_id}, sys_id)
         return response
