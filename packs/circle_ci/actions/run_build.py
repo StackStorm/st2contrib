afda9df502353506fda1ca36e24104808af05305
Manas Kelshikar <manasdk@gmail.com>
2016-04-11 17:46:09 -0700

    extend run_build action to accept bulid_parameters

--- packs/circle_ci/actions/run_build.py
+++ packs/circle_ci/actions/run_build.py
@@ -6,7 +6,7 @@ from lib.action import CircleCI
 
 class RunBuild(CircleCI):
 
-    def run(self, project, branch=None, tag=None, vcs_revision=None):
+    def run(self, project, branch=None, tag=None, vcs_revision=None, build_parameters=None):
         """
         Run build for a SHA in project.
         """
@@ -24,6 +24,13 @@ class RunBuild(CircleCI):
             path = 'project/%s' % project
             data = {'tag': tag} if tag else {'revision': vcs_revision}
             data = json.dumps(data)
+
+        # build parameters are pass-trhrough to circleci
+        if build_parameters:
+            if data is None:
+                data = {}
+            data['build_parameters'] = build_parameters
+
 
         response = self._perform_request(path, method='POST', data=data)
 
