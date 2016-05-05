c1d47e5be34d7595cdb09a4782525179ea774af4
Paul Mulvihill <paul.mulvihill@pulsant.com>
2016-05-05 14:29:54 +0100

    merge removal actions to vm_hw_remove

--- packs/vsphere/actions/vm_hw_remove.py
+++ packs/vsphere/actions/vm_hw_remove.py
@@ -18,17 +18,19 @@ from pyVmomi import vim
 from vmwarelib.actions import BaseAction
 
 
-class RemoveVM(BaseAction):
+class VMRemove(BaseAction):
 
     def run(self, vm, delete_permanently):
         si = self.si
 
-        vm_obj = vim.VirtualMachine(vm, stub=si._stub)
+        vm = vim.VirtualMachine(vm, stub=si._stub)
+        if vm.runtime.powerState == vim.VirtualMachine.PowerState.poweredOn:
+            raise Exception("VM Currently Powered On")
         if delete_permanently:
-            task = vm_obj.Destroy_Task()
+            task = vm.Destroy_Task()
             success = self._wait_for_task(task)
         else:
-            vm_obj.UnregisterVM()
+            vm.UnregisterVM()
             success = True
 
         return {"success": success}
