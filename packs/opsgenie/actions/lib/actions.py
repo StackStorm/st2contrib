13a49a824203ed444315a46d401280c2bb9f67b4
Jon Middleton <jon.middleton@pulsant.com>
2016-06-08 12:18:26 +0100

    Add first 4 actions to OpsGenie pack

---
+++ packs/opsgenie/actions/lib/actions.py
@@ -0,0 +1,56 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import requests
+
+from st2actions.runners.pythonrunner import Action
+
+from lib.utils import send_user_error
+
+
+class OpsGenieBaseAction(Action):
+    def __init__(self, config):
+        super(OpsGenieBaseAction, self).__init__(config)
+
+        self.client = None
+        self.session = requests.Session()
+
+        try:
+            self.api_key = self.config["api_key"]
+            self.api_host = self.config["api_host"]
+        except KeyError:
+            raise ValueError("api_key and api_host needs to be configured!")
+
+    def _url(self, uri):
+        """
+        """
+        return "{}/{}".format(self.api_host.rstrip("/"),
+                              uri)
+
+    def _req(self, method, uri, payload={}):
+        """
+        """
+        payload["apiKey"] = self.api_key
+
+        try:
+            r = self.session.request(method,
+                                     self._url(uri),
+                                     params=payload)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError:
+            raise ValueError("HTTP error: {}: '{}'".format(
+                r.status_code, r.text))
+
+        return r.json()
---
+++ packs/opsgenie/tests/test_action_list_users.py
@@ -0,0 +1,61 @@
+# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+
+import requests_mock
+
+from opsgenie_base_test_case import OpsGenieBaseActionTestCase
+
+from list_users import ListUsersAction
+
+class ListUsersTestCase(OpsGenieBaseActionTestCase):
+    __test__ = True
+    action_cls = ListUsersAction
+
+    def test_run_api_404(self):
+        action = self.get_action_instance(self.full_config)
+
+        adapter = requests_mock.Adapter()
+        action.session.mount('mock', adapter)
+        adapter.register_uri('GET',
+                             "mock://api.opsgenie.com/v1/json/user",
+                             status_code=404)
+
+        self.assertRaises(ValueError,
+                          action.run)
+
+    def test_run_invalid_json(self):
+        action = self.get_action_instance(self.full_config)
+
+        adapter = requests_mock.Adapter()
+        action.session.mount('mock', adapter)
+        adapter.register_uri('GET',
+                             "mock://api.opsgenie.com/v1/json/user",
+                             text="{'ffo': bar}")
+
+        self.assertRaises(ValueError,
+                          action.run)
+
+    def test_run_api_success(self):
+        expected = self.load_json("list_users.json")
+
+        action = self.get_action_instance(self.full_config)
+
+        adapter = requests_mock.Adapter()
+        action.session.mount('mock', adapter)
+        adapter.register_uri('GET',
+                             "mock://api.opsgenie.com/v1/json/user",
+                             text=self.get_fixture_content("list_users.json"))
+
+        result = action.run()
+        self.assertEqual(result, expected)
