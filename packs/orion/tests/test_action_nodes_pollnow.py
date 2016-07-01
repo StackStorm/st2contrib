07515fcb3f4a38c6448431bed064e82bf375d038
Jon Middleton <jon.middleton@pulsant.com>
2016-07-01 11:00:38 +0100

    Another linting issue

--- packs/orion/tests/test_action_nodes_pollnow.py
+++ packs/orion/tests/test_action_nodes_pollnow.py
@@ -12,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 
-from mock import MagicMock
-
 from orion_base_action_test_case import OrionBaseActionTestCase
 
 from nodes_pollnow import NodesPollNow
