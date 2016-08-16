d3f872dbce064549a8ff4b83698b6d97bc882638
bigmstone <matthew99@gmail.com>
2016-08-16 18:54:37 -0500

    Regenerated actions with docstring changes

--- packs/vdx/actions/services_vrrp.py
+++ packs/vdx/actions/services_vrrp.py
@@ -1,11 +1,10 @@
 from pynos import device
 from st2actions.runners.pythonrunner import Action
 
-class interface_lacp_timeout(Action):
+class services_vrrp(Action):
     def run(self, **kwargs):
         conn = (str(kwargs.pop('ip')), str(kwargs.pop('port')))
         auth = (str(kwargs.pop('username')), str(kwargs.pop('password')))
         with device.Device(conn=conn, auth=auth) as dev:
-            kwargs['timeout'] = kwargs.pop('lacp_timeout')
-            dev.interface.lacp_timeout(**kwargs)
+            dev.services.vrrp(**kwargs)
         return 0
