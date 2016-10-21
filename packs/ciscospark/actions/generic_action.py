6b00857c549ffd1a1ec1511b80af67001407d68c
Anthony Shaw <anthonyshaw@apache.org>
2016-10-21 13:48:44 +0400

    fix some bugs after final integration testing

--- packs/ciscospark/actions/generic_action.py
+++ packs/ciscospark/actions/generic_action.py
@@ -10,4 +10,6 @@ class GenericAction(CiscoSparkAction):
     def run(self, accessor, method_name, **kwargs):
         a = getattr(self.connection, accessor)
         result = getattr(a, method_name)(**kwargs)
+        if method_name == 'list':
+            result = list(result)  # iterate generator
         return self._parse_result(result)
--- packs/ciscospark/actions/lib/action.py
+++ packs/ciscospark/actions/lib/action.py
@@ -5,7 +5,6 @@ except ImportError:
                'pip install ciscosparkapi')
     raise ImportError(message)
 
-from .ciscospark_parsers import CiscoSparkResultSets
 from st2actions.runners.pythonrunner import Action
 
 __all__ = [
@@ -19,11 +18,7 @@ class CiscoSparkAction(Action):
         self._access_token = self.config['access_token']
         self._base_url = self.config.get('base_url', DEFAULT_BASE_URL)
         self.connect()
-        self._parser = CiscoSparkResultSets()
 
     def connect(self):
         self.connection = CiscoSparkAPI(access_token=self._access_token,
                                         base_url=self._base_url)
-
-    def _parse_result(self, result):
-        return self._parser.formatter(result)
