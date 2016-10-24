83bd72d0e2c3f78e4db3f039ef34401b5d52483b
Martez Reed <martez.reed@kinneygroup.com>
2016-10-24 12:19:04 -0500

    Fix syntax error in Jenkins install_plugin.py
    Added the necessary whitespace in the python file.

--- packs/jenkins/actions/install_plugin.py
+++ packs/jenkins/actions/install_plugin.py
@@ -3,4 +3,4 @@ from lib import action
 
 class InstallPlugin(action.JenkinsBaseAction):
     def run(self, plugin):
-        return self.jenkins.install_plugin(plugin,include_dependencies='True')
+        return self.jenkins.install_plugin(plugin, include_dependencies='True')
