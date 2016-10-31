08675c25c8f402eee509e267ccc151d85a963d51
Tim Braly <tim_braly@yahoo.com>
2016-10-30 22:05:09 -0700

    Update get_sunset.py

--- packs/astral/actions/get_sunset.py
+++ packs/astral/actions/get_sunset.py
@@ -16,4 +16,4 @@ from lib import BaseAction
 
 class GetSunriseAction(BaseAction.BaseAction):
     def run(self):
-        return str(self.sun['sunset']) # pylint: disable=no-member
+        return str(self.sun['sunset'])  # pylint: disable=no-member
