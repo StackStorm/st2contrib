52cfd54c36a5bbd34e08e5a9a1ae44cb594664e7
Mark Boddington <tuxinvader@users.noreply.github.com>
2016-11-04 13:07:09 +0000

    lint

--- packs/consul/actions/query_service.py
+++ packs/consul/actions/query_service.py
@@ -5,7 +5,7 @@ class ConsulQueryServiceAction(action.ConsulBaseAction):
     def run(self, service, tag=None, ports=False):
         index, service = self.consul.catalog.service(service, tag=tag)
         if ports:
-            addresses = ["{}:{}".format(node['Address'],node['ServicePort']) for node in service]
+            addresses = ["{}:{}".format(node['Address'], node['ServicePort']) for node in service]
         else:
             addresses = [node['Address'] for node in service]
         return ','.join(addresses)
--- packs/consul/actions/register_service.py
+++ packs/consul/actions/register_service.py
@@ -4,6 +4,6 @@ from lib import action
 class ConsulRegisterServiceAction(action.ConsulBaseAction):
     def run(self, nid, name, address, port, tags, dc):
 
-        definition = { "Service": name, "Port": port, "Tags": tags }
+        definition = {"Service": name, "Port": port, "Tags": tags}
         result = self.consul.catalog.register(nid, address, service=definition, dc=dc)
         return result
