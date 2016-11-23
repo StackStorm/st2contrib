7ec3f569e801555291aec7749c7c8a36a85f07bf
Tomaz Muraus <tomaz@tomaz.me>
2016-11-23 20:49:25 +0100

    For consistency with metadata, default action argument to false.

--- packs/consul/actions/get.py
+++ packs/consul/actions/get.py
@@ -2,6 +2,6 @@ from lib import action
 
 
 class ConsulGetAction(action.ConsulBaseAction):
-    def run(self, key, recurse, keys):
+    def run(self, key, recurse, keys=False):
         list, value = self.consul.kv.get(key, recurse=recurse, keys=keys)
         return value
